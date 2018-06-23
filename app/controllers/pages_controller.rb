dd10a02860633d1eebc5222df1e8a98a06a71ea4
Jake Tompkins <term171013@gmail.com>
2018-06-23 15:01:08 +0800

    basic UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
+    
+  
+  CATEGORIES = [
+      {
+        name: "Events",
+        url: "events.jpg"
+      },
+      {
+        name: "Short Films",
+        url: "short_films.jpg"
+      },
+
+      {
+        name: "Documentaries",
+        url: "documentaries.jpg"
+      }
+    ]
+  
+  
   def home
+    @categories = CATEGORIES
   end
 end
